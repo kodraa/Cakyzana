d3f67f105aca0dd6512fcec016cbe9e7d75c35fe
Joseph <Josephabou11@gmail.com>
2022-12-04 16:51:57 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Home from "./components/Homepage/Home";
+import Home from "./Components/Homepage/Home";
 
 function App() {
   return (
--- src/components/Homepage/Home.jsx
+++ src/components/Homepage/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ThisMonth from './ThisMonth'
+import EntrepreneursJourney from './EntrepreneursJourney'
 
 function Home() {
   return (
     <>
         <ThisMonth />
+        <EntrepreneursJourney/>
     </>
   )
 }

8eb0e9c7bc3dbd0bcab663a8de6f64136fe896f4
AhmadSherkawi01 <ahmadsherkawi0@gmail.com>
2022-12-04 17:30:52 +0200

    commit

--- src/Components/Homepage/EducateSection.jsx
+++ src/Components/Homepage/EducateSection.jsx
@@ -3,25 +3,42 @@ import { CONSTANTS, BasicContentDiv } from "../../global";
 import EngArTitle from "../globalComponents/EngArTitle";
 import Section from "../globalComponents/Section";
 import styled from "styled-components";
+import video1 from "../../designAssets/Homepage/Educate/video1.png";
+import video2 from "../../designAssets/Homepage/Educate/video2.png";
+import video3 from "../../designAssets/Homepage/Educate/video3.png";
+import ArrowRight from "../../designAssets/Homepage/Educate/ArrowRight.png";
+import ArrowLeft from "../../designAssets/Homepage/Educate/ArrowLeft.png";
 
 function EducateSection() {
   return (
     <Section>
-      <EngArTitle 
+      <EngArTitle
         english={"Educate"}
         arabic={"ثقافة كايكية"}
-        bottom={"-65%"}
-        right={"-95%"}
+        bottom={"-75%"}
+        right={"-100%"}
         arColor={CONSTANTS.pink}
       />
-      <ContentDiv> 
-        
-        <VideoCarouseContainer>
-        <VideoHolder> </VideoHolder>
-        <VideoHolder> </VideoHolder>
-        <VideoHolder> </VideoHolder>
-        </VideoCarouseContainer>
-        
+      <ContentDiv>
+
+        <VideoCarouselContainer>
+        <Arrow className="left" src={ArrowLeft} alt="" />
+
+          <VideoHolder>
+            <Image src={video1} alt="" />
+          </VideoHolder>
+
+          <VideoHolder>
+            <Image src={video2} alt="" />
+          </VideoHolder>
+
+          <VideoHolder>
+            <Image src={video3} alt="" />
+          </VideoHolder>
+
+        <Arrow className="right" src={ArrowRight} alt="" />
+        </VideoCarouselContainer>
+
       </ContentDiv>
     </Section>
   )
@@ -34,16 +51,38 @@ const ContentDiv = styled(BasicContentDiv)`
   align-items: center;
   `
 
-  const VideoCarouseContainer = styled.div`
-  align-self: center;
-  width: 100%;
-  height: 729px;
+const VideoCarouselContainer = styled.div`
+    width: 100%;
+    height: 550px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-wrap: wrap;  
+    position: relative;
   `
 const VideoHolder = styled.div`
-float: left;
-width: 27%;
-height: 90%;
-margin: 2%;
-background-color: orange;
-border-radius: 10px;
-  `
+  min-width: 31%;
+  height: 100%;
+  border-radius: 10px;
+`
+
+const Image = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: contain;
+`
+const Arrow = styled.img`
+  width: 25px;
+  top: 50%;
+  position: absolute;
+  transform: translateY(-50%);
+  cursor: pointer;
+
+  &.left{
+    left: 0;
+  }
+
+  &.right{
+    right: 0;
+  }
+`

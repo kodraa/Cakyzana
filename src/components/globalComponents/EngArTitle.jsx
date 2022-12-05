105c5a24774f6b8ce534cc9891d98710499ca31e
kodraa <mohammad_nour_10@hotmail.com>
2022-12-05 11:34:18 +0200

    added components folder

---
+++ src/components/globalComponents/EngArTitle.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import styled from "styled-components";
+
+function EngArTitle(props) {
+  return (
+    <TitleContainer>
+      <EngTitle
+        arabic={props.arabic}
+        arColor={props.arColor}
+        enColor={props.enColor ? props.enColor : "black"}
+        bottom={props.bottom}
+        right={props.right}
+      >
+        {props.english}
+      </EngTitle>
+    </TitleContainer>
+  );
+}
+
+export default EngArTitle;
+
+const TitleContainer = styled.div`
+  color: white;
+  height: 10rem;
+  width: 100%;
+  display: flex;
+  align-items: center;
+`;
+
+const EngTitle = styled.h2`
+  font-size: 2rem;
+  font-family: "Century Gothic", sans-serif;
+  font-weight: normal;
+  position: relative;
+  font-size: 50px;
+  color: ${(props) => props.enColor};
+
+  &:after {
+    content: "${(props) => props.arabic}";
+    width: max-content;
+    font-size: 2rem;
+    font-family: "Dima";
+    font-weight: normal;
+    font-size: 60px;
+    position: absolute;
+    bottom: ${(props) => props.bottom};
+    right: ${(props) => props.right};
+    color: ${(props) => props.arColor};
+  }
+`;

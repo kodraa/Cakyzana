b193ef061cec1933bd650753b6556e92596f7fde
J0seph711 <josephabou11@gmail.com>
2022-12-06 10:14:11 +0200

    commit

--- src/components/Homepage/Home.jsx
+++ src/components/Homepage/Home.jsx
@@ -12,7 +12,6 @@ function Home() {
         <ThisMonth  />
         <EntrepreneursJourney /> 
         <TheStars />
-        <EntrepreneursJourney /> 
         <EducateSection/>
         <Footer />
     </>

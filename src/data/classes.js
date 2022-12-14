a9c1d26fcf203a21153c9e8bd9fb6014bf0db27e
MayaBsat02 <mayabsat02@gmail.com>
2022-12-14 11:28:41 +0200

    classes data object

--- src/data/classes.js
+++ src/data/classes.js
@@ -30,7 +30,7 @@ export const classesCategories = {
         Description:
           " In this class you will learn how to bake a fluffy and moist chocolate cake that lasts 3 days or more if refrigirated.",
       },
-      Lemon:{
+      Lemon: {
         id: 13,
         title: "Orange Cake",
         image: "",
@@ -39,16 +39,16 @@ export const classesCategories = {
         Description:
           " In this class you will learn how to bake a fluffy and moist chocolate cake that lasts 3 days or more if refrigirated.",
       },
-        Chocolate:{
-            id: 14,
+      Chocolate: {
+        id: 14,
         title: "Chocolate Cake",
         image: "",
         classSection: "Cake Recipes",
         Duration: "30 mins",
         Description:
           " In this class you will learn how to bake a fluffy and moist chocolate cake that lasts 3 days or more if refrigirated.",
-    },
-    DarkChocolate:{
+      },
+      DarkChocolate: {
         id: 15,
         title: "Dark Chocolate Cake",
         image: "",
@@ -56,8 +56,193 @@ export const classesCategories = {
         Duration: "30 mins",
         Description:
           " In this class you will learn how to bake a fluffy and moist chocolate cake that lasts 3 days or more if refrigirated.",
-  },
+      },
     },
-}
-}
-
+  },
+  filling: {
+    id: 2,
+    title: "Filling",
+    items: {
+      CoffeeCream: {
+        id: 20,
+        title: "Coffee Cream",
+        image: "Baking/VanillaCake",
+        classSection: "Cake Recipes",
+        Duration: "30 mins",
+        Description:
+          " In this class you will learn how to bake a fluffy and moist chocolate cake that lasts 3 days or more if refrigirated.",
+      },
+      ToffeeCream: {
+        id: 21,
+        title: "Toffee Cream",
+        image: "",
+        classSection: "Cream",
+        Duration: "30 mins",
+        Description:
+          " In this class you will learn how to bake a fluffy and moist chocolate cake that lasts 3 days or more if refrigirated.",
+      },
+      VannillaCream: {
+        id: 22,
+        title: "Vanilla Cream",
+        image: "Baking/VanillaCake",
+        classSection: "Cream",
+        Duration: "30 mins",
+        Description:
+          " In this class you will learn how to bake a fluffy and moist chocolate cake that lasts 3 days or more if refrigirated.",
+      },
+      StrawberryCream: {
+        id: 23,
+        title: "Strawberry Cream",
+        image: "Baking/VanillaCake",
+        classSection: "Cream",
+        Duration: "30 mins",
+        Description:
+          " In this class you will learn how to bake a fluffy and moist chocolate cake that lasts 3 days or more if refrigirated.",
+      },
+      DarkChocolateCream: {
+        i: 24,
+        title: "Dark Chocolate Cream",
+        image: "Baking/VanillaCake",
+        classSection: "Cake Recipes",
+        Duration: "30 mins",
+        Description:
+          " In this class you will learn how to bake a fluffy and moist chocolate cake that lasts 3 days or more if refrigirated.",
+      },
+      BubbleCream: {
+        id: 25,
+        title: "Bubble Cream",
+        image: "Baking/VanillaCake",
+        classSection: "Cream",
+        Duration: "30 mins",
+        Description:
+          " In this class you will learn how to bake a fluffy and moist chocolate cake that lasts 3 days or more if refrigirated.",
+      },
+    },
+  },
+  fondant: {
+    id: 3,
+    title: "Fondant",
+    items: {
+      orangeFondant: {
+        id: 30,
+        title: "All about Orange Fondant",
+        image: "",
+        classSection: "Fondant",
+        Duration: "15 mins",
+        Description:
+          "In this class you will learn how to match colors with the fondant, in addition to how to color and tint it the right way.",
+      },
+      blueFondant: {
+        id: 31,
+        title: "All about Blue Fondant",
+        image: "",
+        classSection: "Fondant",
+        Duration: "15 mins",
+        Description:
+          "In this class you will learn how to match colors with the fondant, in addition to how to color and tint it the right way.",
+      },
+      pinkFondant: {
+        id: 32,
+        title: "All about Pink Fondant",
+        image: "",
+        classSection: "Fondant",
+        Duration: "15 mins",
+        Description:
+          "In this class you will learn how to match colors with the fondant, in addition to how to color and tint it the right way.",
+      },
+      whiteFondant: {
+        id: 33,
+        title: "All about White Fondant",
+        image: "",
+        classSection: "Fondant",
+        Duration: "15 mins",
+        Description:
+          "In this class you will learn how to match colors with the fondant, in addition to how to color and tint it the right way.",
+      },
+      pearlFondant: {
+        id: 34,
+        title: "All about Pearl Fondant",
+        image: "",
+        classSection: "Fondant",
+        Duration: "15 mins",
+        Description:
+          "In this class you will learn how to match colors with the fondant, in addition to how to color and tint it the right way.",
+      },
+      blackFondant: {
+        id: 35,
+        title: "All about Black Fondant",
+        image: "",
+        classSection: "Fondant",
+        Duration: "15 mins",
+        Description:
+          "In this class you will learn how to match colors with the fondant, in addition to how to color and tint it the right way.",
+      },
+    },
+  },
+  ganache: {
+    id: 4,
+    title: "Ganache",
+    items: {
+      ChocolateGanahe: {
+        id: 40,
+        title: " Chocolate Ganache",
+        image: "",
+        classSection: "Ganache",
+        Duration: "45 mins",
+        Description:
+          "In this class you will learn how to create a buttery and thick ganache cream, and how to use it for crumb coating.",
+      },
+      WhiteGanache: {
+        id: 41,
+        title: " White Ganache",
+        image: "",
+        classSection: "Ganache",
+        Duration: "45 mins",
+        Description:
+          "In this class you will learn how to create a buttery and thick ganache cream, and how to use it for crumb coating.",
+      },
+      DarkChocolateGanache: {
+        id: 42,
+        title: " Dark Chocolate Ganache",
+        image: "",
+        classSection: "Ganache",
+        Duration: "45 mins",
+        Description:
+          "In this class you will learn how to create a buttery and thick ganache cream, and how to use it for crumb coating.",
+      },
+    },
+  },
+  butterCream: {
+    id: 5,
+    title: "Butter Cream",
+    items: {
+      VanillaButterCream: {
+        id: 50,
+        title: "Vanilla Butter Cream",
+        image: "",
+        classSection: "Butter Cream",
+        Duration: "40 mins",
+        Description:
+          "In this class you will learn how to create a buttery and thick butter cream, and how to use it for crumb coating.",
+      },
+      ChocolateButterCream: {
+        id: 51,
+        title: "Chocolate Butter Cream",
+        image: "",
+        classSection: "Butter Cream",
+        Duration: "40 mins",
+        Description:
+          "In this class you will learn how to create a buttery and thick butter cream, and how to use it for crumb coating.",
+      },
+      StrawberryButterCream: {
+        id: 52,
+        title: "Strawberry Butter Cream",
+        image: "",
+        classSection: "Butter Cream",
+        Duration: "40 mins",
+        Description:
+          "In this class you will learn how to create a buttery and thick butter cream, and how to use it for crumb coating.",
+      },
+    },
+  },
+};

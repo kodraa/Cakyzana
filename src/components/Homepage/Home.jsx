622df83fedd8d6f93485581421057c1f47334daf
mohamadfaki <mohamadfaki3@gmail.com>
2023-04-18 11:52:42 +0300

    Update Home.jsx

--- src/components/Homepage/Home.jsx
+++ src/components/Homepage/Home.jsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import EntrepreneursJourney from './EntrepreneursJourney';
-import Landing from './Landing/Landing';
-import ThisMonth from './ThisMonth/ThisMonth';
-import Classes from './Classes/TheClasses';
-import TheStars from './TheStars/TheStars';
-import Footer from '../globalComponents/Footer';
-import Navbar from '../globalComponents/Navbar';
-import SignUp from '../SignUp/SignUp'
+import React from "react";
+import EntrepreneursJourney from "./EntrepreneursJourney";
+import Landing from "./Landing/Landing";
+import ThisMonth from "./ThisMonth/ThisMonth";
+import Classes from "./Classes/TheClasses";
+import TheStars from "./TheStars/TheStars";
+import Footer from "../globalComponents/Footer";
+import Navbar from "../globalComponents/Navbar";
+import SignUp from "../SignUp/SignUp";
+import Chatbot from "../globalComponents/Chatbot";
 
 function Home() {
   return (
     <>
-        <Navbar isHalfWhite isLogoWhite isCartWhite isWhite={window.innerWidth < 768} />
-        <Landing />
-        <ThisMonth  />
-        <Classes isGrey isWhite/>
-        <TheStars />
-        <EntrepreneursJourney isGrey />
-        <Footer />
+      <Navbar
+        isHalfWhite
+        isLogoWhite
+        isCartWhite
+        isWhite={window.innerWidth < 768}
+      />
+      <Landing />
+      <ThisMonth />
+      <Classes isGrey isWhite />
+      <TheStars />
+      <EntrepreneursJourney isGrey />
+      <Footer />
+      <Chatbot />
     </>
-  )
+  );
 }
 
 export default Home;
-

b008ca63b1477a3549ab7bad42ca0f519a8b7d08
mohamadfaki <mohamadfaki3@gmail.com>
2023-04-18 11:51:29 +0300

    Update Landing.jsx

--- src/components/Classes/Landing.jsx
+++ src/components/Classes/Landing.jsx
@@ -11,24 +11,23 @@ import Navbar from "../globalComponents/Navbar";
 import WhiteLeftArrow from "../../designAssets/Classes/Landing/White Left Arrow.png";
 import WhiteRightArrow from "../../designAssets/Classes/Landing/White Right Arrow.png";
 import LandingCake from "./LandingCake";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper";
+import "swiper/css";
+import "swiper/swiper-bundle.css";
+import "swiper/css/hash-navigation";
+import { useRef } from "react";
 
 function Landing() {
+  const sliderRef = useRef(null);
+
   return (
     <LandingSection>
       <Navbar isLogoWhite isWhite isCartWhite />
       <LandingBackground src={ClassesLanding} alt="Classes Landing" />
       <ContentContainerWrapper>
-        {/* <Carousel itemsToShow={1} renderArrow={myArrow}> */}
-          {/* <img src={WhiteLeftArrow} alt="White Left Arrow" /> */}
-          {/* <LandingCake 
-            Title="Ahlan Bel Eid Cake Workshop"
-            Subtitle="3D and 2D Figures Workshop"
-            Date= "Saturday - Dec 3 - 2022"
-            Time= "From 11:00 am till 2:00 pm"
-            Place= "Hamra - The Olive Grove"
-            WorkshopFees= "$70"
-          />
-          <LandingCake 
+        {/* <img src={WhiteLeftArrow} alt="White Left Arrow" /> */}
+        {/* <LandingCake 
             Title="Summer Breeze Cake Workshop"
             Subtitle="Knife Coloring & Ikebana Tutorial"
             Date= "Saturday - Dec 10 - 2022"
@@ -44,8 +43,58 @@ function Landing() {
             Place= "Hamra - The Olive Grove"
             WorkshopFees= "$100"
           /> */}
-          {/* <img src={WhiteRightArrow} alt="White Right Arrow" /> */}
-        {/* </Carousel> */}
+        {/* <img src={WhiteRightArrow} alt="White Right Arrow" /> */}
+        <Arrow
+          className="left-arrow"
+          src={WhiteLeftArrow}
+          // onClick={handlePrev}
+          // className="left"
+        />
+        <Swiper
+          className="global-swiper"
+          ref={sliderRef}
+          // spaceBetween={50}
+          // slidesPerView={3}
+          // onSlideChange={() => console.log("slide change")}
+          onSwiper={(swiper) => console.log(swiper)}
+          modules={[Navigation]}
+          navigation={{
+            nextEl: ".right-arrow",
+            prevEl: ".left-arrow",
+            clickable: true,
+          }}
+          slidesPerView={1}
+          slidesPerGroup={1}
+          // slidesPerView={'auto'}
+        >
+          <SwiperSlide>
+            <LandingCake
+              Title="Ahlan Bel Eid Cake Workshop"
+              Subtitle="3D and 2D Figures Workshop"
+              Date="Saturday - Dec 3 - 2022"
+              Time="From 11:00 am till 2:00 pm"
+              Place="Hamra - The Olive Grove"
+              WorkshopFees="$70"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <LandingCake
+              Title="Ahlan Bel Eid Cake Workshop"
+              Subtitle="3D and 2D Figures Workshop"
+              Date="Saturday - Dec 3 - 2022"
+              Time="From 11:00 am till 2:00 pm"
+              Place="Hamra - The Olive Grove"
+              WorkshopFees="$70"
+            />
+          </SwiperSlide>
+        </Swiper>
+        {/* <button onClick={() => console.log(mappedElements)}>log elements</button> */}
+        <Arrow
+          className="right-arrow"
+          // onClick={handleNext}
+          src={WhiteRightArrow}
+          // className="right"
+        />
       </ContentContainerWrapper>
     </LandingSection>
   );
@@ -80,7 +129,7 @@ const LandingBackground = styled.img`
   height: 100%;
   width: 100%;
   object-fit: 100%;
-  z-index: -1;
+  /* z-index: -1; */
 `;
 
 const ContentContainerWrapper = styled.div`
@@ -90,14 +139,34 @@ const ContentContainerWrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  /* z-index: -1 !important; */
 
-  & .rec-arrow {
-    border: none !important;
-    outline: none !important;
-    background: none;
+  & .left-arrow,
+  & .right-arrow {
+    width: 25px;
+    cursor: pointer;
   }
 
-  & .rec-dot {
-    display: none;
+  & .global-swiper {
+    height: 100%;
+    position: relative;
+
+    width: 100%;
+
+    /* width: 100%; */
+    /* background-color: red; */
+  }
+`;
+
+const Arrow = styled.img`
+  width: 25px;
+  cursor: pointer;
+
+  &.left {
+    left: 0;
+  }
+
+  &.right {
+    right: 0;
   }
 `;
